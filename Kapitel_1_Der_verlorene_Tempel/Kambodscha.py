c47d3cc2dfb68fcfd93b599a50e32155fce53aa6
Kaiser08Jonas <137834128+Kaiser08Jonas@users.noreply.github.com>
2023-07-16 20:41:48 +0200

    added a few crossings

--- Kapitel_1_Der_verlorene_Tempel/Kambodscha.py
+++ Kapitel_1_Der_verlorene_Tempel/Kambodscha.py
@@ -8,10 +8,12 @@ coloured_west = Fore.BLUE + 'Westen' + Fore.RESET
 coloured_look_around = Fore.BLUE + 'Umschauen' + Fore.RESET
 coloured_arun = Fore.BLUE + 'Arun' + Fore.RESET
 
+
 class Kambodscha:
 
     current_chapter = ''
     talked_to_arun = False
+    visited_stonehill = False
 
     def __init__(self, player_name: str):
         self.player_name = player_name
@@ -27,12 +29,20 @@ class Kambodscha:
               'Stonehill ist ein kleines Dorf, welches am Dschungel von Kambodscha liegt.\n'
               'Es hat nur noch wenige Einwohner, da es nicht sehr viele menschen gibt, welche so abgeschieden von der Civilisation leben möchten\n'
               'Als du dort an kommst und durch das Dorf gehst fällt dir auf, dass bei ein paar Hütten die Fenster zugenagelt wurden.\n'
-              'Was willst du tun?')
+              '') if not self.visited_stonehill else None
+        print('Was willst du tun?')
         choice = input('> ')
+        self.visited_stonehill = True
         match choice:
             case 'Norden':
-                print('Norden')
-                Kambodscha.stonehill(self)
+                print('Im Norden endet das Dorf und der Dschungel beginnt. Willst du dort hin gehen? Ja oder Nein')
+                choice = input('> ')
+                if choice == 'Ja':
+                    print('"Ich bin bereit in den Dschungel zu gehen."\n')
+                    Kambodscha.crossing1(self)
+                else:
+                    print('"Ich gehe zurück ins Dorf."')
+                    Kambodscha.stonehill(self)
             case 'Süden':
                 print('Im Süden des Dorfes ist der Pier, an welchem du abgesetzt wurdest. Als du ausgestiegen bist, hast du dich bereits\n'
                       'umgesehen und nichts auffälliges entdeckt.')
@@ -70,9 +80,9 @@ class Kambodscha:
                               f'[{coloured_arun}] "Kurz vorher habe ich es beobachtet und ein Glänzendes Objekt gesehen. Das suche ich,"\n'
                               f'[{coloured_arun}] "doch meine Eltern erlauben mir nicht, an die Küste zu gehen und nachzuschauen,"\n'
                               f'[{coloured_arun}] "da dort eine große Krabbe ist, welche sehr aggressive seien soll."\n'
-                              f'[{coloured_arun}] "Wonach Suchen sie denn?\n'
-                              f'[{self.player_name}] "Ich suche nach einem Amulett. Ein paar Eingeborenen sollen wissen, wo es ist.\n'
-                              f'[{coloured_arun}] "Sie sollten sich von ihnen fern halten.\n'
+                              f'[{coloured_arun}] "Wonach Suchen sie denn?"\n'
+                              f'[{self.player_name}] "Ich suche nach einem Amulett. Ein paar Eingeborenen sollen wissen, wo es ist."\n'
+                              f'[{coloured_arun}] "Sie sollten sich von ihnen fern halten."\n'
                               f'Kaum hatte Arun den Satz ausgesprochen, rannte ehr zurück ins Haus und verschloss die Tür.\n'
                               f'"Habe ich was falsches gesagt?"')
                         self.talked_to_arun = True
@@ -88,3 +98,69 @@ class Kambodscha:
             case _:
                 print('falsch')
                 Kambodscha.stonehill(self)
+
+    def crossing1(self):
+        self.current_chapter = 'crossing1'
+        print(f'Eine Kreuzung. Dort vorne steht ein Wegweiser:\n{coloured_north} = Dschungel\n{coloured_south} = Stonehill\n{coloured_west} = Küste\n'
+              f'"Was soll ich tun?')
+        choice = input('> ')
+        match choice:
+            case 'Norden':
+                print('"Ich gehe tiefer in den Dschungel hinein."')
+                Kambodscha.crossing2(self)
+            case 'Süden':
+                print('"Ich gehe zurück ins Dorf."')
+                Kambodscha.stonehill(self)
+            case 'Osten':
+                print('Im Osten ist der Dschungel zu dicht, um ihn zu durchqueren.')
+                Kambodscha.crossing1(self)
+            case 'Westen':
+                print(f'"Ich versuche mal mein Glück im {coloured_west}."')
+                Kambodscha.coast(self)
+            case 'Umschauen':
+                print('Es ist nur dichter Dschungel und Stonehill zu sehen.')
+                Kambodscha.crossing1(self)
+
+    def crossing2(self):
+        self.current_chapter = 'crossing2'
+        print(f'"Hier ist nur dichter Dschungel. In der Ferne sehe ich ein paar Häuser von Stonehill, welche im {coloured_south} sind."')
+        choice = input('> ')
+        match choice:
+            case 'Norden':
+                print('"Am besten gehe ich in den Norden."')
+                Kambodscha.crossing3(self)
+            case 'Süden':
+                print('"Ich gehe näher zu den Häusern von Stonehill."')
+                Kambodscha.crossing1(self)
+            case 'Osten':
+                print('"Dort ist extrem dichter Dschungel. Ich glaube, dass ich dort nicht hindurch komme."')
+                Kambodscha.crossing2(self)
+            case 'Westen':
+                print('')
+            case 'Umschauen':
+                print(f'"Ich sehe nur dichten Dschungel und in der Ferne Stonehill. Im {coloured_north} und {coloured_west} sieht der Dschungel"\n'
+                      f'"Durchquerbar aus. Vielleicht sollte ich es dort versuchen."')
+                Kambodscha.crossing2(self)
+
+    def crossing3(self):
+        self.current_chapter = 'crossing3'
+        print(f'"Schon wieder nur dichter Dschungel. Irgendwo muss es doch etwas geben, was mir weiter hilft."')
+        choice = input('> ')
+        match choice:
+            case 'Norden':
+                print(f'"Im {coloured_north} gibt es keinen Weg, durch welchen ich den Dschungel weiter durchqueren könnte."')
+                Kambodscha.crossing3(self)
+            case 'Süden':
+                print('"Ich gehe nach Süden"')
+                Kambodscha.crossing2(self)
+            case 'Osten':
+                print('')
+            case 'Westen':
+                print(f'')
+            case 'Umschauen':
+                print(f'"Hier gibt es wieder nur Dschungel. Wobei ich im {coloured_east} etwas durch die Bäume zu sehen.\n'
+                      f'Ich kann nicht genau erkennen, was es ist."')
+                Kambodscha.crossing3(self)
+
+    def coast(self):
+        self.current_chapter = 'coast'
